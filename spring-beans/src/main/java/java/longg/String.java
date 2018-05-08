f3dc58c35a9a71f28ac32210f916a073b92ddcb9
unknown <wangyongchao@eetop.com>
2018-05-08 18:57:51 +0800

    new file:   pom.xml 	new file:   spring-beans/pom.xml 	new file:   spring-beans/spring-beans.iml 	new file:   spring-beans/src/main/java/java/longg/String.java 	new file:   spring-beans/target/classes/java/longg/String.class 	new file:   spring-beans/target/maven-archiver/pom.properties 	new file:   spring-beans/target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst 	new file:   spring-beans/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst 	new file:   spring-beans/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/inputFiles.lst 	new file:   spring-beans/target/spring-beans-1.0-SNAPSHOT.jar 	new file:   spring-context/pom.xml

---
+++ spring-beans/src/main/java/java/longg/String.java
@@ -0,0 +1,14 @@
+package java.longg;
+
+/**
+ * @author wangyongchao
+ * @date 2018/5/8  16:07
+ */
+public class String {
+
+    public static void main(String[] args){
+
+      System.out.println("heloow");
+    }
+}
+
